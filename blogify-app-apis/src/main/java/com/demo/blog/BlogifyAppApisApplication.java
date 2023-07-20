b373188261adfb0e9905ed7e16ddf713f3ed120a
sambanna07 <115611654+sambanna07@users.noreply.github.com>
2023-07-20 10:00:47 +0530

    basic authentication
    basic form based and basic

--- blogify-app-apis/src/main/java/com/demo/blog/BlogifyAppApisApplication.java
+++ blogify-app-apis/src/main/java/com/demo/blog/BlogifyAppApisApplication.java
@@ -1,12 +1,16 @@
 package com.demo.blog;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class BlogifyAppApisApplication {
+public class BlogifyAppApisApplication{
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogifyAppApisApplication.class, args);
@@ -16,5 +20,6 @@ public class BlogifyAppApisApplication {
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
+
 
 }
--- blogify-app-apis/src/main/java/com/demo/blog/entity/User.java
+++ blogify-app-apis/src/main/java/com/demo/blog/entity/User.java
@@ -1,9 +1,11 @@
 package com.demo.blog.entity;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -12,7 +14,14 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.demo.blog.payload.CommentsDTO;
 
@@ -26,31 +35,66 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
-public class User {
-	
+public class User implements UserDetails {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "user_id",nullable = false)
-	private int id;	
-	
-	@Column(name="user_name")
+	@Column(name = "user_id", nullable = false)
+	private int id;
+
+	@Column(name = "user_name")
 	private String name;
-	
-	@Column(name="user_email")
+
+	@Column(name = "user_email")
 	private String email;
-	
-	@Column(name="user_password")
+
+	@Column(name = "user_password")
 	private String password;
-	
-	@Column(name="user_about")
+
+	@Column(name = "user_about")
 	private String about;
-	
-	@OneToMany(mappedBy = "user",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
-	private List<Post> posts=new ArrayList<>();
-	
-	@OneToMany(mappedBy = "user",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
-	private Set<Comments> comments=new HashSet<>();
 
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private List<Post> posts = new ArrayList<>();
 
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private Set<Comments> comments = new HashSet<>();
+
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user", referencedColumnName = "user_id"), inverseJoinColumns = @JoinColumn(name = "role", referencedColumnName = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<SimpleGrantedAuthority> authories = this.roles.stream()
+				.map((role) -> new SimpleGrantedAuthority(role.getRole())).collect(Collectors.toList());
+		return authories;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 
 }
